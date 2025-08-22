d2b022f4a77bfaf144ecf2f6f6a8879c6fa31f2c
maeurer.alexander@gmail.com <maeurer.alexander@gmail.com>
2025-08-22 13:26:10 +0200

    basic JavaScript

---
+++ breakout/scripts/game.js
@@ -0,0 +1,3 @@
+function gameStart(e) {
+  e.preventDefault();
+}
---
+++ breakout/scripts/setup.js
@@ -0,0 +1,12 @@
+const gameElem = document.getElementById("game");
+const ballElem = document.getElementById("ball");
+const paddleElem = document.getElementById("paddle");
+
+const scoreDisplayElem = document.getElementById("game-display-score");
+const livesDisplayElem = document.getElementById("game-display-lives");
+
+const screenGameOverElem = document.getElementById("game-screen-gameover");
+
+const btnStartGame = document.getElementById("game-btn-start");
+
+btnStartGame.addEventListener("click", gameStart(e));
