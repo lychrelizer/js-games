cba1c9315f6cfade9e4c93f8fd0778cd42550f2e
maeurer.alexander@gmail.com <maeurer.alexander@gmail.com>
2025-08-22 16:22:47 +0200

    add paddle and ball movement logic

--- breakout/scripts/game.js
+++ breakout/scripts/game.js
@@ -1,8 +1,70 @@
 function resetGame() {
+  game.lives = MAX_LIVES;
+  game.score = 0;
+  game.gameOver = false;
+  game.started = false;
+  scoreDisplayElem.innerText = game.score;
+  livesDisplayElem.innerText = game.lives;
   screenGameOverElem.style.display = "none";
+  animationRepeat = 0;
 }
 
 function gameStart(e) {
   e.preventDefault();
   resetGame();
+  cancelAnimationFrame(animationRepeat);
+  animationRepeat = requestAnimationFrame(update);
+}
+
+function update() {
+  if (game.gameOver === false) {
+    let pCurrent = paddleElem.offsetLeft;
+
+    if (paddleElem.left && paddleElem.offsetLeft > +25) {
+      pCurrent -= 5;
+    } else if (
+      paddleElem.right &&
+      paddleElem.offsetLeft < containerDim.width - paddleElem.offsetWidth - 25
+    ) {
+      pCurrent += 5;
+    }
+
+    paddleElem.style.left = pCurrent + "px";
+
+    if (!game.started) {
+      waitingOnPaddle();
+    } else {
+      ballMove();
+    }
+
+    animationRepeat = requestAnimationFrame(update);
+  }
+}
+
+function waitingOnPaddle() {
+  ballElem.style.top = paddleElem.offsetTop - ballElem.offsetHeight + "px";
+  ballElem.style.left =
+    paddleElem.offsetLeft +
+    paddleElem.offsetWidth / 2 -
+    ballElem.offsetWidth / 2 +
+    "px";
+}
+
+function ballMove() {
+  let x = ballElem.offsetLeft;
+  let y = ballElem.offsetTop;
+
+  x += game.ballDir[0];
+  y += game.ballDir[1];
+
+  if (x > containerDim.width - ballElem.offsetWidth - 25 || x < 25) {
+    game.ballDir[0] = game.ballDir[0] * -1;
+  }
+
+  if (y > containerDim.height - ballElem.offsetHeight - 25 || y < 25) {
+    game.ballDir[1] = game.ballDir[1] * -1;
+  }
+
+  ballElem.style.top = y + "px";
+  ballElem.style.left = x + "px";
 }
--- breakout/scripts/setup.js
+++ breakout/scripts/setup.js
@@ -1,12 +1,41 @@
 const gameElem = document.getElementById("game");
 const ballElem = document.getElementById("ball");
 const paddleElem = document.getElementById("paddle");
-
 const scoreDisplayElem = document.getElementById("game-display-score");
 const livesDisplayElem = document.getElementById("game-display-lives");
-
 const screenGameOverElem = document.getElementById("game-screen-gameover");
-
 const btnStartGame = document.getElementById("game-btn-start");
 
+// basic game setup
+const MAX_LIVES = 3;
+
+const game = {
+  gameOver: true,
+  started: false,
+  score: 0,
+  lives: MAX_LIVES,
+  ballDir: [5, 5, 5],
+};
+
+let animationRepeat;
+let containerDim = gameElem.getBoundingClientRect();
+
+console.log(containerDim);
+
+// set events
 btnStartGame.addEventListener("click", gameStart);
+
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  let key = e.key;
+  console.log(key);
+  if (key === "ArrowLeft") paddleElem.left = true;
+  else if (key === "ArrowRight") paddleElem.right = true;
+  else if (key === " ") game.started = true;
+});
+document.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  let key = e.key;
+  if (key === "ArrowLeft") paddleElem.left = false;
+  else if (key === "ArrowRight") paddleElem.right = false;
+});
